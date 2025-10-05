49e700a16d58db424f7a7012fe2d359d508580f3
Frank Bonah-Okyere <bonahokyerefrank@gmail.com>
2025-10-05 21:52:58 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500;600&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased')}>
+      <body className={cn('font-body antialiased')} suppressHydrationWarning>
         <AuthProvider>
           {children}
           <Toaster />
