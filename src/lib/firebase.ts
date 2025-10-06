da182de8ea44894ba6a1106cf8555dfb3647df39
Frank Bonah-Okyere <bonahokyerefrank@gmail.com>
2025-10-06 15:16:28 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-firebase-api-key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "your-firebase-auth-domain",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-firebase-project-id",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "your-firebase-storage-bucket",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "your-firebase-messaging-sender-id",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-firebase-app-id",
+  apiKey: "AIzaSyD0WeJZ1axm_xzHbDNpM2pG4LJvr_4uwkQ",
+  authDomain: "codeloop-5c108.firebaseapp.com",
+  projectId: "codeloop-5c108",
+  storageBucket: "codeloop-5c108.appspot.com",
+  messagingSenderId: "31617736587",
+  appId: "1:31617736587:web:37b5b5a0150e721ba5198e",
+  measurementId: "G-NJ08PQDBRS"
 };
 
 let app;
